1aeb6eeba7ed96d0733f4f33e2cbb8c2fa7f3488
harsh -uranium <harshshrivastav139@gmail.com>
2022-04-14 01:45:28 +0530

    harsh

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+//const UserController= require("../controllers/userController")
+const BookController = require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
+router.get("/test-me", function(req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
-
-router.post("/createBook", BookController.createBook  )
+router.post("/createBook", BookController.createBook)
 
 router.get("/getBooksData", BookController.getBooksData)
+
+router.get("/bookbyChetanBhagat", BookController.bookbyChetanBhagat)
+
+router.get("/findandUpdateTwoStates", BookController.findandUpdateTwoStates)
+
+router.get("/findBook", BookController.findBook)
 
 module.exports = router;
